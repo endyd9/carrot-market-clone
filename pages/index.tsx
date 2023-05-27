0b942ca0760f80ff8d99f4d6adedb9bf0fc2260c
endyd9 <endyd9@gmail.com>
2023-05-28 04:11:59 +0900

    2일차

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,126 +2,79 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <form className="flex flex-col space-y-2 bg-blue-500 p-5 focus-within:bg-blue-100">
-      <input
-        type="text"
-        required
-        placeholder="UserName"
-        className="invalid:bg-red-500"
-      />
-      <input type="password" required placeholder="PassWord" />
-      <input type="submit" value="Login" className="bg-white" />
-    </form>
+    <div className="flex flex-col space-y-5 py-10">
+      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+        <div
+          key={i}
+          className="flex px-4 border-b pb-4 cursor-pointer justify-between"
+        >
+          <div className="flex space-x-4">
+            <div className="w-20 h-20 bg-gray-400 rounded-md" />
+            <div className="pt-2 flex flex-col">
+              <h3 className="text-sm font-medium text-gray-900">
+                New iPhone 14
+              </h3>
+              <span className="text-xs text-gray-500">Black</span>
+              <span className="font-medium mt-1 text-gray-900">$95</span>
+            </div>
+          </div>
+          <div className="flex space-x-2 items-end justify-end">
+            <div className="flex space-x-0.5 items-center text-sm text-gray-600">
+              <svg
+                className="w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                ></path>
+              </svg>
+              <span>1</span>
+            </div>
+            <div className="flex space-x-0.5 items-center text-sm text-gray-600">
+              <svg
+                className="w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                ></path>
+              </svg>
+              <span>1</span>
+            </div>
+          </div>
+        </div>
+      ))}
+      <button className="fixed hover:bg-orange-500 transition-colors bottom-28 right-5 bg-orange-400 sahdow-xl rounded-full p-4 text-white">
+        <svg
+          className="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+      </button>
+    </div>
   );
 };
 
 export default Home;
-
-{
-  /* <div className="bg-slate-400 py-20 px-10 grid gap-10 min-h-screen">
-      <div className="bg-white p-6 rounded-3xl shadow-2xl">
-        <span className="font-semibold text-3xl">Select Item</span>
-        <ul>
-          {[1, 2, 3, 4, 5].map((i) => (
-            <div
-              key={i}
-              className="flex justify-between my-2 odd:bg-red-500 even:bg-blue-500"
-            >
-              <span className="text-gray-500">Gray Chair</span>
-              <span className="font-semibold">$170</span>
-            </div>
-          ))}
-        </ul>
-        {["a", "b", "c", ""].map((c, i) => (
-          <li className="bg-red-500 empty:hidden" key={i}>
-            {c}
-          </li>
-        ))}
-        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
-          <span>Total</span>
-          <span className="font-semibold">$970</span>
-        </div>
-        <button
-          className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-4/6 mx-auto
-         hover:bg-teal-500 hover:text-black
-          active:bg-yello-500
-          focus:bg-red-500"
-        >
-          Checkout
-        </button>
-      </div>
-
-      <div className="bg-white overflow-hidden rounded-3xl shadow-2xl group">
-        <div className="bg-blue-500 p-6 pb-14">
-          <span className="text-white text-2xl">Profile</span>
-        </div>
-        <div className="rounded-3xl p-6 bg-white relative -top-5">
-          <div className="flex relative -top-16 items-end justify-between">
-            <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Orders</span>
-              <span className="font-medium">340</span>
-            </div>
-            <div className="h-24 w-24 bg-red-400 rounded-full group-hover:bg-blue-500 transition-colors" />
-            <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Spent</span>
-              <span className="font-medium">$2,310</span>
-            </div>
-          </div>
-        </div>
-        <div className="relative -mt-1 -mb-16 -top-24 flex flex-col items-center ">
-          <span className=" text-lg font-medium">Tony Molloy</span>
-          <span className="text-sm text-gray-500">New York, USA</span>
-        </div>
-      </div>
-
-      <div className="bg-white p-6 rounded-2xl shadow-2xl">
-        <div className="flex mb-5 justify-between items-center">
-          <span>⬅️</span>
-          <div className="space-x-3">
-            <span>★4.9</span>
-            <span className="shadow-xl p-2 rounded-md">💖</span>
-          </div>
-        </div>
-        <div className="bg-zinc-400 h-64 mb-5" />
-        <div className="flex flex-col">
-          <span className="font-medium text-xl">Swoon Lounge</span>
-          <span className="text-xs text-gray-500">Chair</span>
-          <div className="mt-3 mb-5 flex justify-between items-center">
-            <div className="space-x-1.5">
-              <button
-                className="w-5 h-5 rounded-full bg-yellow-500
-               focus:ring-2 ring-offset-2 ring-yellow-500
-               transition
-               "
-              />
-              <button
-                className="w-5 h-5 rounded-full bg-indigo-500
-               focus:ring-2 ring-offset-2 ring-indigo-500
-               transition"
-              />
-              <button
-                className="w-5 h-5 rounded-full bg-teal-500
-               focus:ring-2 ring-offset-2 ring-teal-500
-               transition"
-              />
-            </div>
-            <div className="flex items-center space-x-3">
-              <button className="bg-blue-200 rounded-md flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
-                -
-              </button>
-              <span>1</span>
-              <button className="bg-blue-200 rounded-md flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
-                +
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between items-center">
-            <span className="font-medium text-2xl">$450</span>
-            <button className="bg-blue-500 py-2 px-8 text-center text-white rounded-lg">
-              Add to cart
-            </button>
-          </div>
-        </div>
-      </div>
-    </div> */
-}
