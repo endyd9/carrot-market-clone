6481064fe80ba30630d8915d509d6e05d4ec3b9f
endyd9 <endyd9@gmail.com>
2023-07-07 18:28:24 +0900

    v1.0

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
-  experimental: {
-    reactRoot: true,
-    runtime: "nodejs",
-    serverComponents: true,
-  },
+  // experimental: {
+  //   reactRoot: true,
+  //   runtime: "nodejs",
+  //   serverComponents: true,
+  // },
   images: {
     domains: ["imagedelivery.net", "videodelivery.net"],
   },
